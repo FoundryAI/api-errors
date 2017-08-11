748b1b1f20fbc9a65bc7be9d4545979e5bd4671c
Jonathan Bender <jlbender@gmail.com>
2017-08-11 11:20:35 -0400

    add ConflictError docs, export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { AuthenticationError } from './authenticationError';
 export { BadRequestError } from './badRequestError';
+export { ConflictError } from './conflictError';
 export { ForbiddenError } from './forbiddenError';
 export { InternalError } from './internalError';
 export { NotFoundError } from './notFoundError';
